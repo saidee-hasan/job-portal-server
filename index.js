f4d3464667ff559dfe8b476f4e895cd0c189f389
saideeWD <mdsaideehasan@gmail.com>
2024-12-28 11:01:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app =express();
+const port = process.env.PORT || 5000;
+app.use(express())
+app.use(cors())
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnyhf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send("Server is running ")
+})
+
+app.listen(port,()=>{
+    console.log(`Job is Counting ${port}`)
+
+})
